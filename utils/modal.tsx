e137f81abeadd516e4da80d66c85909dc4b22061
agredo-alejo <agredoalejo@gmail.com>
2022-05-01 20:11:49 -0400

    added utils folder

---
+++ utils/modal.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode, useRef } from 'react'
+import { useClickOutside } from './clickOutside'
+
+interface ModalProps {
+    children: ReactNode | ReactNode[]
+    show: boolean
+    handleClose: () => void
+}
+function Modal({ children, show, handleClose }: ModalProps) {
+
+    const modalRef = useRef(null)
+    useClickOutside(modalRef, handleClose)
+
+    if (!show) return <></>
+
+    return (
+        <div className='modal'>
+            <span ref={modalRef}>
+                {children}
+            </span>
+        </div>
+    )
+}
+
+export default Modal
