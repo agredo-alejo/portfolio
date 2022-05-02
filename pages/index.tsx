b33d8b8b174a7623cc2ace3e98144344b3bd14c2
agredo-alejo <agredoalejo@gmail.com>
2022-05-02 14:58:23 -0400

    added og meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,15 +14,23 @@ const Home: NextPage = () => {
   const scroll = () => {
     scrollRef.current?.scrollIntoView({ behavior: "smooth" })
   }
+  const title = "Alejandro Agredo | Web Developer"
+  const description = "A web developer fascinated by problem-solving and creating interactive graphic interfaces"
+  const keywords = "Portfolio, Web Developer, Web, Developer, Tyescript, Node, Front-end, developer, React, NextJS, HTML, CSS, Alejandro Agredo, Alejandro, Agredo, JavaScript"
   return (
     <div className='h-full'>
       <Head>
-        <title>Alejandro Agredo | Web Developer</title>
-        <meta name="description" content="A web developer fascinated by problem-solving and creating interactive graphic interfaces" />
-        <meta name="keywords" content="Portfolio, Web Developer, Tyescript, Node, Front-end, developer, React, NextJS, HTML, CSS, JavaScript" />
+        <title> {title} </title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta itemProp='description' content={description} />
         <meta name="author" content="Alejandro Agredo" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta property='og:url' content="https://www.alejandroagredo.com/" />
+        <meta property="og:title" content={title} />
+        <meta property='og:description' content={description} />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Canvas />
 
