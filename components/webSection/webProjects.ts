c4f11382b91eb0afa6c82e7a7a5f3da4939f25c0
agredo-alejo <agredoalejo@gmail.com>
2022-05-01 21:12:21 -0400

    included public folder

--- components/webSection/webProjects.ts
+++ components/webSection/webProjects.ts
@@ -1,22 +1,22 @@
 const webProjects = [
     {
-        desk: "/webimg/jiyaki2.png",
-        phone: "/webimg/jiy6.png",
+        desk: "/webimg/h_jiyaki.png",
+        phone: "/webimg/v_jiyaki.png",
         link: "https://jiyaki.web.app/",
         github: "https://github.com/agredo-alejo/jiyaki",
         title: "Jiyaki",
         techs: ["React", "Tyescript", "Sass", "Firebase"]
     }, {
-        desk: "/webimg/whats2.png",
-        phone: "/webimg/whats3.png",
+        desk: "/webimg/h_chat.png",
+        phone: "/webimg/v_chat.png",
         link: "https://whatsapp-clone-275.web.app/",
         github: "https://github.com/agredo-alejo/whatsapp-clone",
         title: "Chat App",
         techs: ["React", "Tyescript", "Sass", "Firebase"]
     },
     {
-        desk: "/webimg/products7.png",
-        phone: "/webimg/productFrame.png",
+        desk: "/webimg/h_inventory.png",
+        phone: "/webimg/v_inventory.png",
         link: "https://products-inventory-275.web.app/stats",
         github: "https://github.com/agredo-alejo/products-inventory",
         title: "Inventory Management",
---
+++ public/libraries/README.md
@@ -0,0 +1,6 @@
+# Libraries
+
+-functions.js: A set of functions with the main goal of facilitating the use of the html5 canvas, heavily inspired by p5.js.
+-nd.min.js: A machine learning library that provides a way to work with data types like tensors, does not currently use the GPU, heavily inspired by tensorflow
+-tf.min.js: Tensorflow for javascript, 2019
+-vectors.js: A library that provides a way to work with vectors, used as a kind of kernel for the basic physics engine used in my projects 
