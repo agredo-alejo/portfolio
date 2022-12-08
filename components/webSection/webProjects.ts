06d47ff10357393bfe6f90ab750eae6b777a26ad
agredo-alejo <agredoalejo@gmail.com>
2022-12-07 19:44:07 -0500

    added ama y liberate web development project

--- components/webSection/webProjects.ts
+++ components/webSection/webProjects.ts
@@ -1,26 +1,35 @@
 const webProjects = [
-    {
-        desk: "/webimg/h_jiyaki.png",
-        phone: "/webimg/v_jiyaki.png",
-        link: "https://jiyaki.web.app/",
-        github: "https://github.com/agredo-alejo/jiyaki",
-        title: "Jiyaki",
-        techs: ["React", "Tyescript", "Sass", "Firebase"]
-    }, {
-        desk: "/webimg/h_chat.png",
-        phone: "/webimg/v_chat.png",
-        link: "https://apollochat.vercel.app/",
-        github: "https://github.com/agredo-alejo/apollochat",
-        title: "Apollo Chat",
-        techs: ["NextJs", "Tyescript", "TailwindCss", "Firebase"]
-    },
-    {
-        desk: "/webimg/h_inventory.png",
-        phone: "/webimg/v_inventory.png",
-        link: "https://products-inventory-275.web.app/stats",
-        github: "https://github.com/agredo-alejo/products-inventory",
-        title: "Inventory Management",
-        techs: ["React", "Tyescript", "Sass", "Firebase"]
-    }
-]
-export default webProjects
+  {
+    desk: "/webimg/h_amayliberate.png",
+    phone: "/webimg/v_amayliberate.png",
+    link: "https://www.amayliberate.com/",
+    github: "https://github.com/agredo-alejo/amayliberate",
+    title: "Ama Y Libérate",
+    techs: ["NextJs", "MailerLite", "Stripe", "Tyescript", "TailwindCss"],
+  },
+  {
+    desk: "/webimg/h_jiyaki.png",
+    phone: "/webimg/v_jiyaki.png",
+    link: "https://jiyaki.web.app/",
+    github: "https://github.com/agredo-alejo/jiyaki",
+    title: "Jiyaki",
+    techs: ["React", "Tyescript", "Sass", "Firebase"],
+  },
+  {
+    desk: "/webimg/h_chat.png",
+    phone: "/webimg/v_chat.png",
+    link: "https://apollochat.vercel.app/",
+    github: "https://github.com/agredo-alejo/apollochat",
+    title: "Apollo Chat",
+    techs: ["NextJs", "Tyescript", "TailwindCss", "Firebase"],
+  },
+  {
+    desk: "/webimg/h_inventory.png",
+    phone: "/webimg/v_inventory.png",
+    link: "https://products-inventory-275.web.app/stats",
+    github: "https://github.com/agredo-alejo/products-inventory",
+    title: "Inventory Management",
+    techs: ["React", "Tyescript", "Sass", "Firebase"],
+  },
+];
+export default webProjects;
