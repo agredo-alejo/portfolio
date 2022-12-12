01dcb1c40dd488efb263a5bb13302f2f009069f0
agredo-alejo <agredoalejo@gmail.com>
2022-12-11 23:24:02 -0500

    support for language translation

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,28 +2,31 @@ import React, { MouseEventHandler } from "react";
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 
 interface HeaderProps {
-  scroll: MouseEventHandler<HTMLButtonElement>;
+   scroll: MouseEventHandler<HTMLButtonElement>;
 }
 function Header({ scroll }: HeaderProps) {
-  return (
-    <header className="relative w-full h-screen flex flex-col justify-center items-center text-[#eee]">
-      <span className="flex flex-col items-center max-w-[60ch]">
-        <h1 className="text-4xl text-center w-fit">
-          Hi, I am Alejandro Agredo
-        </h1>
-        <p className="mt-2 text-xl text-center">
-          A web developer fascinated by problem solving and creating interactive
-          graphic interfaces
-        </p>
-      </span>
-      <button
-        onClick={scroll}
-        className="border-none mt-4 bg-[#80808080] hover:shadow-[0_0_.2rem_#808080] rounded-full flex p-1"
-      >
-        <KeyboardDoubleArrowDownIcon />
-      </button>
-    </header>
-  );
+   return (
+      <header className="relative w-full h-screen flex flex-col justify-center items-center text-[#eee]">
+         <span className="flex flex-col items-center max-w-[60ch]">
+            <h1 className=" max-w-[90vw] flex flex-wrap justify-center gap-2 text-4xl text-center">
+               <p className="capitalize">Hi,</p>
+               <p>I am</p>
+               <p className="capitalize">Alejandro</p>
+               <p className="capitalize">Agredo</p>
+            </h1>
+            <p className="mt-2 text-xl text-center">
+               A web developer fascinated by problem solving and creating
+               interactive graphic interfaces
+            </p>
+         </span>
+         <button
+            onClick={scroll}
+            className="border-none mt-4 bg-[#80808080] hover:shadow-[0_0_.2rem_#808080] rounded-full flex p-1"
+         >
+            <KeyboardDoubleArrowDownIcon />
+         </button>
+      </header>
+   );
 }
 
 export default Header;
--- components/webSection/projectInfo.tsx
+++ components/webSection/projectInfo.tsx
@@ -3,54 +3,53 @@ import { BiLinkExternal } from "react-icons/bi";
 import { FiGithub } from "react-icons/fi";
 
 export interface ProjectProps {
-  project: {
-    desk: string;
-    phone: string;
-    link: string;
-    github: string;
-    title: string;
-    techs: string[];
-  };
+   project: {
+      desk: string;
+      phone: string;
+      link: string;
+      github: string;
+      title: string;
+      techs: string[];
+   };
 }
 
 function ProjectInfo({ project }: ProjectProps) {
-  return (
-    <div className="flex flex-col max-w-xl p-2 mb-4 ml-4 text-gray-200 sm:w-1/2">
-      <div className="flex items-center justify-between w-full mb-2 mr-4">
-        <h2 className="text-2xl">{project.title}</h2>
-        <span className="flex items-center justify-center text-xl">
-          <a
-            className="flex "
-            href={project.github}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FiGithub className="hover:drop-shadow-[.1rem_0_.1rem_gray]" />
-          </a>
-          <a
-            className="flex ml-1"
-            href={project.link}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BiLinkExternal className="hover:drop-shadow-[.1rem_0_.1rem_gray]" />
-          </a>
-        </span>
-      </div>
+   return (
+      <div className="flex flex-col max-w-xl p-2 mb-4 ml-4 text-gray-200 sm:w-1/2">
+         <div className="flex items-center justify-between w-full mb-2 mr-4">
+            <h2 className="text-2xl notranslate">{project.title}</h2>
+            <span className="flex items-center justify-center text-xl">
+               <a
+                  className="flex "
+                  href={project.github}
+                  target="_blank"
+                  rel="noreferrer"
+               >
+                  <FiGithub className="hover:drop-shadow-[.1rem_0_.1rem_gray]" />
+               </a>
+               <a
+                  className="flex ml-1"
+                  href={project.link}
+                  target="_blank"
+                  rel="noreferrer"
+               >
+                  <BiLinkExternal className="hover:drop-shadow-[.1rem_0_.1rem_gray]" />
+               </a>
+            </span>
+         </div>
 
-      <span className="flex flex-wrap border-b-2 border-[#444] ">
-        {project.techs.map((tech: string, key: number) => (
-          <p
-            key={key}
-            className="mr-2 mb-2 px-2 bg-[#0000001a] text-white cursor-default"
-          >
-            {" "}
-            {tech}{" "}
-          </p>
-        ))}
-      </span>
-    </div>
-  );
+         <span className="flex flex-wrap border-b-2 border-[#444] ">
+            {project.techs.map((tech: string) => (
+               <p
+                  key={tech}
+                  className="mr-2 mb-2 px-2 bg-[#0000001a] text-white cursor-default notranslate"
+               >
+                  {tech}
+               </p>
+            ))}
+         </span>
+      </div>
+   );
 }
 
 export default ProjectInfo;
